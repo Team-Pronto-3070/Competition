b83036366e14ac7faf488ddf7a6624bf9064b18a
Pronto Laptop 2 <cthegoldstick@gmail.com>
2014-02-17 10:11:12 -0800

    sttopoisopujisj github

--- src/edu/wpi/first/wpilibj/templates/CentralCode.java
+++ src/edu/wpi/first/wpilibj/templates/CentralCode.java
@@ -88,87 +88,78 @@ public class CentralCode extends IterativeRobot {
         conf = 0;
         relay.set(Relay.Value.kOn);
         noWait = 0;
-<<<<<<< HEAD
-=======
         sol1.set(true); //change it to fast setting
         sol2.set(false);
         sol4.set(true);
         sol5.set(false);
         sol7.set(true);
         sol8.set(false);
->>>>>>> a927a343140ea489bb9f2b2b59942b391938cda3
     }
 
     public void autonomousPeriodic() {
-        if (!ready){
-        conf = conf + SmartDashboard.getNumber("Confidence") - 70;
-<<<<<<< HEAD
-        }
-=======
->>>>>>> a927a343140ea489bb9f2b2b59942b391938cda3
-        if (ultrasonic.getVoltage() <= .96) {
-            jag1.set(0);
-            jag3.set(0);
-            ready = true;
-        } else {
-            jag1.set(1);
-            jag3.set(-1);
-        }
-        if (i >= 100) {
-            goShoot = false;
-<<<<<<< HEAD
-            sol7.set(false);
-            sol8.set(true);
-            i = 0;
-        }
-        if (ready && conf >= 40) {
-            goShoot = true;
-        }
-        if (ready && conf < 40) {
-            noWait++;
-            if (noWait >= 40 && noWait < 70) {
-                jag1.set(-1);
-                jag3.set(1);
-            }
-            if (noWait >= 70 && noWait < 110) {
+        if (!ready) {
+            conf = conf + SmartDashboard.getNumber("Confidence") - 70;
+            if (ultrasonic.getVoltage() <= .96) {
                 jag1.set(0);
                 jag3.set(0);
-            }
-            if (noWait >= 110 && noWait < 150) {
+                ready = true;
+            } else {
                 jag1.set(1);
                 jag3.set(-1);
             }
-            if (noWait == 150) {
+            if (i >= 100) {
+                goShoot = false;
+                sol7.set(false);
+                sol8.set(true);
+                i = 0;
+            }
+            if (ready && conf >= 40) {
                 goShoot = true;
             }
-        }
-        if (goShoot) {
-            sol7.set(true);
-            sol8.set(false);
-=======
-            sol7.set(true);
-            sol8.set(false);
-            i = 0;
-        }
-        if (ready && conf >= 40) {
-            goShoot = true;
-        }
-        if (ready && conf < 40) {
-            noWait++;
-        }
-        if (ready && conf < 40 && noWait == 150) {
-            goShoot = true;
-        }
-        if (goShoot && i < 3) {
-            i++;
-            sol4.set(false);
-            sol5.set(true);
-        }
-        if (goShoot && i >= 3) {
-            sol7.set(false);
-            sol8.set(true);
->>>>>>> a927a343140ea489bb9f2b2b59942b391938cda3
-            i++;
+            if (ready && conf < 40) {
+                noWait++;
+                if (noWait >= 40 && noWait < 70) {
+                    jag1.set(-1);
+                    jag3.set(1);
+                }
+                if (noWait >= 70 && noWait < 110) {
+                    jag1.set(0);
+                    jag3.set(0);
+                }
+                if (noWait >= 110 && noWait < 150) {
+                    jag1.set(1);
+                    jag3.set(-1);
+                }
+                if (noWait == 150) {
+                    goShoot = true;
+                }
+            }
+            if (goShoot) {
+                sol7.set(true);
+                sol8.set(false);
+                sol7.set(true);
+                sol8.set(false);
+                i = 0;
+            }
+            if (ready && conf >= 40) {
+                goShoot = true;
+            }
+            if (ready && conf < 40) {
+                noWait++;
+            }
+            if (ready && conf < 40 && noWait == 150) {
+                goShoot = true;
+            }
+            if (goShoot && i < 3) {
+                i++;
+                sol4.set(false);
+                sol5.set(true);
+            }
+            if (goShoot && i >= 3) {
+                sol7.set(false);
+                sol8.set(true);
+                i++;
+            }
         }
     }
 
@@ -182,24 +173,19 @@ public class CentralCode extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
-<<<<<<< HEAD
-        SmartDashboard.putNumber("Distance in.", 102.4*ultrasonic.getAverageVoltage());
-                //^need to do this as a boolean eventually
-=======
+        SmartDashboard.putNumber("Distance in.", 102.4 * ultrasonic.getAverageVoltage());
+        //^need to do this as a boolean eventually
         if (digi3.get()) {
             SmartDashboard.putBoolean("ArmBack", true);
         }
         if (!digi3.get()) {
             SmartDashboard.putBoolean("ArmBack", false);
         }
-        SmartDashboard.putNumber("Distance in.", 102.4 * ultrasonic.getVoltage());
-        //^need to do this as a boolean eventually
->>>>>>> a927a343140ea489bb9f2b2b59942b391938cda3
     }
 
     public void disabledInit() {
         drive.setRun(false);
-        
+
         loadAndShoot.setRun(false);
     }
 
